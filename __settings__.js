bfc0ef3094cc11bfaa0210f914a283f354c233ef
JaskaStudiosGmbH <105844607+JaskaStudiosGmbH@users.noreply.github.com>
2022-06-16 20:40:38 +1000

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "924961.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 31118502, 31114312, 31114085, 34411089, 31118254, 31118819, 31181188, 31194229, 31196328, 31197367, 34291990, 34408273, 34408274, 34408280, 34452252, 34457221, 34489585, 34507690, 34658896, 34768118, 34776756, 34820075, 34923052, 34931646, 35317275 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/31194025/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/31194026/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/31194024/1/ammo.js', 'preload' : true},
+];
